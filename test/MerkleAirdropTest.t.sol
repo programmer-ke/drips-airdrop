40b6242330f7d8816c3a504aed81284b0bae96b8
Kenny Rch <ken@99nth.com>
2026-06-23 10:42:01 +0300

    feat: allow signature based claim authorization

--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -20,12 +20,15 @@ contract MerkleAirdropTest is Test {
     address user;
     uint256 userPrivateKey;
 
+    address public gasPayer;
+
     bytes32 proofOne = 0x875631ab70d5c9a1430b5a44e60c2c218f68a62a01a73b2e49d03f130b04b5c9;
     bytes32 proofTwo = 0x0fb85f7b6df160de3a55fbbc3757e1166f70d574c0b5520e22040ad2b88d7a5d;
     bytes32[] public PROOF = [proofOne, proofTwo];
 
     function setUp() public {
         (user, userPrivateKey) = makeAddrAndKey("USER");
+        gasPayer = makeAddr("gasPayer");
 
         deployer = new DeployMerkleAirdrop();
         (token, airdrop) = deployer.run();
@@ -34,12 +37,18 @@ contract MerkleAirdropTest is Test {
     function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
 
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_PER_USER, PROOF);
+        // let user sign message
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_PER_USER);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_PER_USER, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
 
-        assertEq(endingBalance - startingBalance, AMOUNT_PER_USER, "User did not receive claim amount");
+        assertEq(
+            endingBalance - startingBalance, AMOUNT_PER_USER, "User did not receive claim amount"
+        );
     }
 }
 
